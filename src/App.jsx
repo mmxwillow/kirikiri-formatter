ef99ca8ae562fddd4438d55409f9c851bb191953
Monika <mxwierzba@gmail.com>
2024-11-30 17:09:06 +0100

    i was tired don't judge me

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import JSZip from 'jszip';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [zipFile, setZipFile] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  async function readFile(event) {
+      let zip = new JSZip();
+
+      for(let i = 0; i < event.target.files.length; i++) {
+        const file = event.target.files[i];
+
+        if (file) {
+          const text = await new Response(file).text();
+          const fileName = file.name.substring(0, file.name.lastIndexOf('.'));
+          
+          let extractNames = text.replace(/\[名前 n = "([^"]+)"\]/g, '$1:');
+          const removeExtraData = extractNames.replace(/\[.*?\]/g, "");
+          const final = removeExtraData.replace(/\s\s+/g, '\n');
+
+          zip.file(`${fileName}_formatted.txt`, final);
+        }
+      }
+      setZipFile(zip);
+      setLoaded(true);
+  }
+
+  async function saveFile() {
+    const fileToDownload =  await zipFile.generateAsync({
+      type: "blob",
+      streamFiles: true
+    })
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(fileToDownload);
+    link.download = `formatted_files.zip`
+    link.click();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Kirikiri script file cleaner</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input type='file' onChange={(e) => {readFile(e), setLoaded(false)}} multiple/>
+        { loaded && <button style={{marginTop:20}} onClick={saveFile}>Download .zip</button> }
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          *for now you have to copy the contents of a .ks file to a new .txt file and then upload it here
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
