50033d96024f6c0d40c149c8ff00b66112cab643
Monika <mxwierzba@gmail.com>
2025-04-09 23:15:02 +0200

    jp support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import JSZip from 'jszip';
 import { useState } from 'react';
+import iconv from 'iconv-lite';
 
 function App() {
   const [zipFile, setZipFile] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [isJP, setIsJP] = useState(false);
 
   async function readFile(event) {
       let zip = new JSZip();
@@ -13,12 +15,25 @@ function App() {
         const file = event.target.files[i];
 
         if (file) {
-          const text = await new Response(file).text();
-          const fileName = file.name.substring(0, file.name.lastIndexOf('.'));
+          // Read the file as an ArrayBuffer
+          const arrayBuffer = await file.arrayBuffer();
+
+          let text = "";
+          if(isJP){
+            const buffer = new Uint8Array(arrayBuffer);
+            text = iconv.decode(buffer, 'Windows932');
+          } else {
+            const decoder = new TextDecoder('utf-16le');
+            text = decoder.decode(arrayBuffer);
+          }
           
-          let extractNames = text.replace(/\[名前 n = "([^"]+)"\]/g, '$1:');
+          const fileName = file.name.substring(0, file.name.lastIndexOf('.'));
+
+          console.log(text);
+
+          let extractNames = text.replace(/\[名前 n\s?=\s?"([^"]+)"\]/g, '$1:');
           const removeExtraData = extractNames.replace(/\[.*?\]/g, "");
-          const final = removeExtraData.replace(/\s\s+/g, '\n');
+          const final = removeExtraData.replace(/\s\s+/g, '\n\n');
 
           zip.file(`${fileName}_formatted.txt`, final);
         }
@@ -43,11 +58,12 @@ function App() {
     <>
       <h1>Kirikiri script file cleaner</h1>
       <div className="card">
+        <p>
+          <input type="checkbox" id="isJP" name="isJP" value="isJP" checked={isJP} onChange={() => setIsJP(!isJP)}/>
+          <label htmlFor="isJP">Check before uploading files if they're in Japanese</label>
+        </p>
         <input type='file' onChange={(e) => {readFile(e), setLoaded(false)}} multiple/>
         { loaded && <button style={{marginTop:20}} onClick={saveFile}>Download .zip</button> }
-        <p>
-          *for now you have to copy the contents of a .ks file to a new .txt file and then upload it here
-        </p>
       </div>
     </>
   )
